8532aa539ea06bb6c67f9cb9b8cf0938f829e17d
SudhanshuVishwakarma <Vsudhanshu18@gmail.com>
2022-07-14 17:12:27 +0530

    commit

---
+++ Loops.py
@@ -0,0 +1,63 @@
+from itertools import count
+from multiprocessing.sharedctypes import Value
+import numbers
+from unicodedata import name
+
+# count = 0
+# while count < 5:
+#     print(count)
+#     count = count + 1
+#     if count == 3:
+#         break
+
+# count = 0
+# while count < 5:
+#     if count == 5:
+#         continue
+#     print(count)
+#     count = count + 1
+
+
+# numbers = [0, 1, 2, 3, 4, 5]
+# for number in numbers:
+#     print(number)
+
+# language = "python"
+# for letter in language:
+#     print(letter)
+# for i in range(len(language)):
+#     print(language[i])
+
+# dictonary
+# for itrator in dct:
+#     code goes here
+
+# person = {
+#     'first_name': 'Asabeneh',
+#     'last_name': 'Yetayeh',
+#     'age': 250,
+#     'country': 'Finland',
+#     'is_marred': True,
+#     'skills': ['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+#     'address': {
+#         'street': 'Space street',
+#         'zipcode': '02210'
+#     }
+# }
+
+# for key in person:
+#     print(key)
+# for key, Value in person.items():
+#     print(key, Value)
+
+# it_comp={"f",'a','g'}
+# for comp in it_comp:
+#     print(comp)
+
+# numbers = (1, 2, 3, 5, 46)
+# for number in numbers:
+#     print(number)
+#     if number == 3:
+#         continue
+#     print("next number should be", number+1) if number != 6 else print("loop's end")
+# print('outside')
---
+++ range.py
@@ -0,0 +1,9 @@
+# The range() function is used list of numbers.
+#  the range(start, end, increment) // default start= 0 incr=1;
+
+lst = list(range(11))
+print(lst)
+st = set(range(1, 11))
+print(st)
+
+# Sudhanshu vi9shwakarnna
